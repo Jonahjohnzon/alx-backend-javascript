26dfd29fb36db4ab20a393c48beeddda8f7cde49
jonah ominyi <jonahjohnzon@gmail.com>
2024-06-04 20:24:30 +0000

    psh

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
@@ -13,29 +12,29 @@ const countStudents = (dataPath) => {
     .toString('utf-8')
     .trim()
     .split('\n');
-  const studentGroups = {};
+  const studentGroup = {};
   const dbFieldNames = fileLines[0].split(',');
-  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+  const studentPropName = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
   for (const line of fileLines.slice(1)) {
     const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
     const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(studentGroups).includes(field)) {
-      studentGroups[field] = [];
+    if (!Object.keys(studentGroup).includes(field)) {
+      studentGroup[field] = [];
     }
-    const studentEntries = studentPropNames
+    const studentEntries = studentPropName
       .map((propName, idx) => [propName, studentPropValues[idx]]);
-    studentGroups[field].push(Object.fromEntries(studentEntries));
+    studentGroup[field].push(Object.fromEntries(studentEntries));
   }
 
   const totalStudents = Object
-    .values(studentGroups)
+    .values(studentGroup)
     .reduce((pre, cur) => (pre || []).length + cur.length);
   console.log(`Number of students: ${totalStudents}`);
-  for (const [field, group] of Object.entries(studentGroups)) {
-    const studentNames = group.map((student) => student.firstname).join(', ');
-    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+  for (const [field, group] of Object.entries(studentGroup)) {
+    const studentN = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentN}`);
   }
 };
 
