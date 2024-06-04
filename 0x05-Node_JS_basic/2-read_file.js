66a77e22ca67c1a5e1ead8997bf0183d72e57a18
jonah ominyi <jonahjohnzon@gmail.com>
2024-06-04 20:17:52 +0000

    psh

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -13,29 +13,29 @@ const countStudents = (dataPath) => {
     .toString('utf-8')
     .trim()
     .split('\n');
-  const studentGroup = {};
+  const studentGroups = {};
   const dbFieldNames = fileLines[0].split(',');
-  const studentPropName = dbFieldNames.slice(0, dbFieldNames.length - 1);
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
   for (const line of fileLines.slice(1)) {
     const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
     const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(studentGroup).includes(field)) {
-      studentGroup[field] = [];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
     }
-    const studentEntries = studentPropName
+    const studentEntries = studentPropNames
       .map((propName, idx) => [propName, studentPropValues[idx]]);
-    studentGroup[field].push(Object.fromEntries(studentEntries));
+    studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
   const totalStudents = Object
-    .values(studentGroup)
+    .values(studentGroups)
     .reduce((pre, cur) => (pre || []).length + cur.length);
   console.log(`Number of students: ${totalStudents}`);
-  for (const [field, group] of Object.entries(studentGroup)) {
-    const studentN = group.map((student) => student.firstname).join(', ');
-    console.log(`Number of students in ${field}: ${group.length}. List: ${studentN}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
   }
 };
 
