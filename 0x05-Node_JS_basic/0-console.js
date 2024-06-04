50351e03982ae6480cf7c23cf1494c7fff2d3213
jonah ominyi <jonahjohnzon@gmail.com>
2024-06-04 20:00:36 +0000

    push

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Log a message to  console.
+ * @param {string} message - message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Export display Message function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage;
