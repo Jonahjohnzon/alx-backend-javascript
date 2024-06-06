bf0012788dcfe8878f68f5f77602531cd7e4c7ad
jonah ominyi <jonahjohnzon@gmail.com>
2024-06-06 19:51:45 +0000

    pi

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express();
+const PORT = 7865;
 
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.listen(7865, () => {
+app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
 
